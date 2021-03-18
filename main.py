afa15e5541e8baa398cba9366fd06da4fe5d97f1
purplecoder04 <purple_lover_04@msn.com>
2021-03-18 01:28:48 +0000

    works great

--- main.py
+++ main.py
@@ -2,16 +2,20 @@ def is_leap(year):
   if year % 4 == 0:
     if year % 100 == 0:
       if year % 400 == 0:
-        print("Leap year.")
+        return True
       else:
-        print("Not leap year.")
+        return False 
     else:
-      print("Leap year.")
+      return True
   else:
-    print("Not leap year.")
+    return False
 
-def days_in_month():
+def days_in_month(year_entered, month_entered):
   month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
+  if is_leap(year_entered):
+    month_days[1] += 1
+  return month_days[month_entered - 1]
+
   
   
 #🚨 Do NOT change any of the code below 
